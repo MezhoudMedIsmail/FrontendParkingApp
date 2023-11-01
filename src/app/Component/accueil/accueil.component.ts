017a12038bc151771cf8015741d48f656b5cf3dc
m521 <mohamedismail.mezhoud@esprit.tn>
2023-11-01 17:39:04 +0100

    ProjetPFELastComm

--- src/app/Component/accueil/accueil.component.ts
+++ src/app/Component/accueil/accueil.component.ts
@@ -1,26 +1,47 @@
 import { Component } from '@angular/core';
+import { PlaceParkingComponent } from '../parking/place-parking/place-parking.component';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { PlaceParkingService } from 'src/app/Service/place-parking.service';
 import { TokenService } from 'src/app/Service/token.service';
 import { UserServiceService } from 'src/app/Service/user.service';
+import { ParkingService } from 'src/app/Service/parking.service';
 const LogoImgPath =
   'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp';
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.css']
+  styleUrls: ['./accueil.component.css'],
 })
 export class AccueilComponent {
   imageProfile!: SafeUrl;
   listHistory : any[] = [];
   userId: string = '';
+  ListParkings!: any[];
+  placeParkings :any[] =[];
 
-  constructor(private placeService : PlaceParkingService,private tokenService : TokenService,private service: UserServiceService,private sanitizer: DomSanitizer){
+
+  constructor(private placeService : PlaceParkingService,private tokenService : TokenService,private service: UserServiceService,private sanitizer: DomSanitizer,private sservice: ParkingService,private ssservice: PlaceParkingService){
     this.getHistory();
     this.userId = localStorage.getItem('id') as string;
     this.getImage(this.userId as string);
 
   }
+
+  get(){
+    this.ssservice.get().subscribe((res:any)=>{
+      this.placeParkings = res;
+      console.log(res)
+    });
+  }
+
+  getParkings(){
+    this.sservice.get().subscribe((res:any)=>{
+      this.ListParkings = res;
+      console.log(res)
+    });
+  }
+
+
 
   getImage(userId: string) {
     this.service.getFile(userId).subscribe(
